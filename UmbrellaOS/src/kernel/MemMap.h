df2c7b40c434c420454c15f7d1907fbbb4b68935
Florian Schumacher <FloCo12@gmx.net>
2022-08-25 08:35:42 +0200

    Changed the source files from cpp to c.

--- UmbrellaOS/src/kernel/MemMap.h
+++ UmbrellaOS/src/kernel/MemMap.h
@@ -1,32 +1,12 @@
 #pragma once
 #include "Types.h"
 
-enum class RegionType : UDWORD
-{
-	Usable = 1,
-	Reserved = 2,
-	ACPI_Reclaimable = 3,
-	ACPI_NVS = 4,
-	BadMemory = 5
-};
-
 #pragma pack(push, 1)
-union ExtendedAttributes
-{
-	struct
-	{
-		UDWORD ignore : 1;
-		UDWORD nonVolatile : 1;
-		UDWORD unused : 30;
-	};
-	UDWORD value;
-};
-
-struct MemMapEntry
+typedef struct
 {
 	UQWORD baseAddr;
 	UQWORD length;
-	RegionType type;
-	ExtendedAttributes exAttrib;
-};
+	UDWORD type;
+	UDWORD exAttrib;
+} MemMapEntry;
 #pragma pack(pop)
--- UmbrellaOS/src/kernel/Terminal.h
+++ UmbrellaOS/src/kernel/Terminal.h
@@ -1,23 +1,18 @@
 #pragma once
 #include "Types.h"
 
-#define VGA_MEM reinterpret_cast<volatile char*>(0xB8000)
+#define VGA_MEM ((volatile char*)0xB8000)
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_ELMSIZE 2
 #define VGA_STRIDE (VGA_WIDTH * VGA_ELMSIZE)
 #define VGA_SIZE (VGA_STRIDE * VGA_HEIGHT)
 
-class Terminal
-{
-public:
-	static void Clear(BYTE color);
-	static void SetCursor(WORD x, WORD y);
-	static void HideCursor();
-	static void EnableCursor(WORD start, WORD end);
-	static void PutString(WORD x, WORD y, const char* str, BYTE color);
-	static WORD GetCursorPos();
-	static void Print(const char* str, BYTE color);
-	static void Scroll(BYTE color);
-};
-
+void Clear(BYTE color);
+void SetCursor(WORD x, WORD y);
+void HideCursor();
+void EnableCursor(WORD start, WORD end);
+void PutString(WORD x, WORD y, const char* str, BYTE color);
+WORD GetCursorPos();
+void Print(const char* str, BYTE color);
+void Scroll(BYTE color);
